ba7ca924340a203ae33d833ced615c3bce55bf5a
alfredo aguirre <alfredodejesusaguirre@yahoo.com>
2015-10-18 22:45:18 -0500

    remove space

--- AWSLambda/index.js
+++ AWSLambda/index.js
@@ -1,6 +1,5 @@
 // For development/testing purposes
 exports.handler = function (event, context) {
-
   console.log( "Running index.handler" );
   console.log( "==================================");
   console.log( "event", event );
